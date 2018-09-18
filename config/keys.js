12b7b10d015bf598bc5fe1be53e9fd4150c4add4
Sean Cahill <seancahill25@gmail.com>
2018-09-18 12:41:55 -0400

    added user authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Sean:sean25@ds259802.mlab.com:59802/codeconnections"
+  mongoURI: "mongodb://Sean:sean25@ds259802.mlab.com:59802/codeconnections",
+  secretOrKey: "secret"
 };
